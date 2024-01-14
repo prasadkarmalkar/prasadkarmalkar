22d445ee4528aaf925749965c7bd6e6adc152ebd
Prasad Karmalkar <prasadkarmalkar2020@gmail.com>
2024-01-14 18:11:08 +0530

    Add contact us functionality

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,32 @@
 import GradientText from '@/components/Utils/GradientText'
 import React from 'react'
+import { Resend } from 'resend';
 
 function page() {
+    async function sendEmail(formData) {
+        'use server'
+
+        const name = formData.get('name')
+        const email = formData.get('email')
+        const message = formData.get('message')
+        const resend = new Resend(process.env.RESEND_API);
+
+        resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: 'prasadkarmalkar2020@gmail.com',
+            subject: `Got message from ${name}`,
+            html: `
+            Name: ${name} <br>
+            Email: ${email} <br>
+            Message:
+            <p>${message}</p>`
+        });
+
+    }
     return (
         <section >
             <h2 className='text-3xl sm:text-5xl md:text-6xl xl:text-7xl text-center font-semibold leading-snug mt-24 max-w-screen-lg m-auto'>Let's make the <GradientText>world</GradientText> a better place <GradientText>together.</GradientText></h2>
-            <form action="" className='max-w-screen-sm m-auto mt-20 flex flex-col gap-10 '>
+            <form action={sendEmail} className='max-w-screen-sm m-auto mt-20 flex flex-col gap-10 '>
                 <div>
                     <label htmlFor="name">Name</label>
                     <input className='mt-2 w-full text-white bg-transparent border rounded-lg text-lg p-2' type="text" name="name" id="name" placeholder='Enter Your Name' />
