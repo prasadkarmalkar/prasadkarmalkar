dba969273fd2fdea0ebe0088a81de0a4a2698a78
PRASAD KARMALKAR <prasadkarmalkar2020@gmail.com>
2024-11-10 15:41:06 +0530

    Fix border issue for tech stack in about page

--- src/components/About/TechStack.tsx
+++ src/components/About/TechStack.tsx
@@ -9,7 +9,7 @@ function TechStack() {
 		<section className='mt-20 max-w-screen-lg m-auto'>
 			<h2 className='text-center text-3xl sm:text-4xl '>Tech Stack & <strong><GradientText>Mastery</GradientText></strong></h2>
 			<div className='mt-20 grid grid-cols-1 grid-rows-7 sm:grid-cols-8 sm:grid-rows-6 gap-10 justify-center'>
-				<div className={` ${gradientBorder} relative row-span-2 sm:row-span-2 sm:col-span-8 lg:col-span-5 lg:row-span-4  bg-gradient-to-r from-gray-950 to-gray-900 rounded-2xl`}>
+				<div className={`after:content-[''] after:h-full after:w-full after:absolute after:bg-gradient-to-r after:top-0 after:p-2 after:scale-[1.01] after:-z-10 after:rounded-2xl after:from-pink-500 after:to-violet-600 relative row-span-2 sm:row-span-2 sm:col-span-8 lg:col-span-5 lg:row-span-4  bg-gradient-to-r from-gray-950 to-gray-900 rounded-2xl`}>
 					<h3 className='text-2xl sm:text-3xl my-6 text-center font-semibold '>
 						FrontEnd
 					</h3>
@@ -37,7 +37,7 @@ function TechStack() {
 						</li>
 					</ul>
 				</div>
-				<div className={` ${gradientBorder} relative row-span-1 sm:col-span-4 sm:row-span-1 lg:col-span-3 lg:row-span-2 bg-gradient-to-r from-gray-950 to-gray-900  rounded-2xl`}>
+				<div className={`after:content-[''] after:h-full after:w-full after:absolute after:bg-gradient-to-r after:top-0 after:p-2 after:scale-[1.01] after:-z-10 after:rounded-2xl after:from-pink-500 after:to-violet-600 relative row-span-1 sm:col-span-4 sm:row-span-1 lg:col-span-3 lg:row-span-2 bg-gradient-to-r from-gray-950 to-gray-900  rounded-2xl`}>
 					<h3 className='text-2xl sm:text-3xl my-6 text-center font-semibold '>
 						BackEnd
 					</h3>
@@ -50,7 +50,7 @@ function TechStack() {
 						</li>
 					</ul>
 				</div>
-				<div className={`${gradientBorder} relative row-span-1 sm:col-span-4 sm:row-span-1 lg:col-span-3 lg:row-span-2 bg-gradient-to-r from-gray-950 to-gray-900  rounded-2xl`}>
+				<div className={`after:content-[''] after:h-full after:w-full after:absolute after:bg-gradient-to-r after:top-0 after:p-2 after:scale-[1.01] after:-z-10 after:rounded-2xl after:from-pink-500 after:to-violet-600 relative row-span-1 sm:col-span-4 sm:row-span-1 lg:col-span-3 lg:row-span-2 bg-gradient-to-r from-gray-950 to-gray-900  rounded-2xl`}>
 					<h3 className='text-2xl sm:text-3xl my-6 text-center font-semibold '>
 						Databases
 					</h3>
@@ -63,7 +63,7 @@ function TechStack() {
 						</li>
 					</ul>
 				</div>
-				<div className={`${gradientBorder} relative row-span-2 sm:col-span-8 sm:row-span-2 lg:col-span-6 lg:row-span-2 bg-gradient-to-r from-gray-950 to-gray-900  rounded-2xl`}>
+				<div className={`after:content-[''] after:h-full after:w-full after:absolute after:bg-gradient-to-r after:top-0 after:p-2 after:scale-[1.01] after:-z-10 after:rounded-2xl after:from-pink-500 after:to-violet-600 relative row-span-2 sm:col-span-8 sm:row-span-2 lg:col-span-6 lg:row-span-2 bg-gradient-to-r from-gray-950 to-gray-900  rounded-2xl`}>
 					<h3 className='text-2xl sm:text-3xl my-6 text-center font-semibold '>
 						Tools
 					</h3>
@@ -85,7 +85,7 @@ function TechStack() {
 						</li>
 					</ul>
 				</div>
-				<div className={`${gradientBorder} relative row-span-1 sm:col-start-3 sm:row-span-1 sm:col-end-7 lg:col-span-2 lg:row-span-2 bg-gradient-to-r from-gray-950 to-gray-900  rounded-2xl`}>
+				<div className={`after:content-[''] after:h-full after:w-full after:absolute after:bg-gradient-to-r after:top-0 after:p-2 after:scale-[1.01] after:-z-10 after:rounded-2xl after:from-pink-500 after:to-violet-600 relative row-span-1 sm:col-start-3 sm:row-span-1 sm:col-end-7 lg:col-span-2 lg:row-span-2 bg-gradient-to-r from-gray-950 to-gray-900  rounded-2xl`}>
 					<h3 className='text-2xl sm:text-3xl my-6 text-center font-semibold'>
 						CMS
 					</h3>
